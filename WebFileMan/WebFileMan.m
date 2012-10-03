40e61ca001c8ab1bb50f1c283ca7408f2bffe9e9
Luis Ibanez <luis.ibanez@kitware.com>
2012-10-02 20:00:54 -0400

    Add top FileMan menu options

--- WebFileMan/WebFileMan.m
+++ WebFileMan/WebFileMan.m
@@ -1,10 +1,24 @@
 getTreeMenu(sessid)
  n menu
  ;
- s menu(1,"text")="Option 1"
+ s menu(1,"text")="1 ENTER OR EDIT FILE ENTRIES"
  s menu(1,"nvp")="Item=1"
- s menu(2,"text")="Option 2"
+ s menu(2,"text")="2 PRINT FILE ENTRIES"
  s menu(2,"nvp")="Item=2"
+ s menu(3,"text")="3 SEARCH FILE ENTRIES"
+ s menu(3,"nvp")="Item=3"
+ s menu(4,"text")="4 MODIFY FILE ATTRIBUTES"
+ s menu(4,"nvp")="Item=4"
+ s menu(5,"text")="5 INQUIRE TO FILE ENTRIES"
+ s menu(5,"nvp")="Item=5"
+ s menu(6,"text")="6 UTILITY FUNCTIONS"
+ s menu(6,"nvp")="Item=6"
+ s menu(7,"text")="7 OTHER OPTIONS"
+ s menu(7,"nvp")="Item=7"
+ s menu(8,"text")="8 DATA DICTIONNARY UTILITIES"
+ s menu(8,"nvp")="Item=8"
+ s menu(9,"text")="9 TRANSFER ENTRIES"
+ s menu(9,"nvp")="Item=9"
  d mergeArrayToSession^%zewdAPI(.menu,"myTreeMenu",sessid)
  QUIT ""
 
