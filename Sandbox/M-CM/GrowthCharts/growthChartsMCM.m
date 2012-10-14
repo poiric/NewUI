681410ee8cec7ce2e11d3fe78df8d16d59f5bf03
Luis Ibanez <luis.ibanez@kitware.com>
2012-10-14 17:58:44 -0600

    Using Weight and Height as labels

--- Sandbox/M-CM/GrowthCharts/growthChartsMCM.m
+++ Sandbox/M-CM/GrowthCharts/growthChartsMCM.m
@@ -1,16 +1,16 @@
-getChartData(sessid) ; 10/14/12 5:19pm
+getChartData(sessid) ; 10/14/12 5:57pm
  new chart
  ;
- set chart("line1",1)=10
- set chart("line1",2)=9
- set chart("line1",3)=5
- set chart("line1",4)=2
- set chart("line1",5)=4.7
- set chart("line2",1)=4
- set chart("line2",2)=3
- set chart("line2",3)=7
- set chart("line2",4)=5
- set chart("line2",5)=7
+ set chart("Weight",1)=10
+ set chart("Weight",2)=9
+ set chart("Weight",3)=5
+ set chart("Weight",4)=2
+ set chart("Weight",5)=4.7
+ set chart("Height",1)=4
+ set chart("Height",2)=3
+ set chart("Height",3)=7
+ set chart("Height",4)=5
+ set chart("Height",5)=7
  set chart("month",1)="Jan"
  set chart("month",2)="Feb"
  set chart("month",3)="Mar"
