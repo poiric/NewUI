4f5294be901d8a83ed9dd280e6d9d3be46f8ac56
Luis Ibanez <luis.ibanez@kitware.com>
2012-10-14 20:01:47 -0600

    Got first test to work

--- Sandbox/M-CM/GrowthCharts/databaseMCM.m
+++ Sandbox/M-CM/GrowthCharts/databaseMCM.m
@@ -1,34 +1,34 @@
-createDB() ; 10/14/12 7:30pm
+createDB() ; 10/14/12 8:00pm
  kill ^zmcmData
  set ^zmcmData=""
  set ^zmcmData("nextChild")=0
  set ^zmcmData("children")=""
- quit ""
+ quit
  ;
 addChild(name,dateOfBirth) ;
  new nextChild
  set nextChild=^zmcmData("nextChild")
  set ^zmcmData("children",nextChild,"name")=""
- set ^zmcmData("children",nextChild,"name","first")=name("first")
- set ^zmcmData("children",nextChild,"name","middle")=name("middle")
- set ^zmcmData("children",nextChild,"name","last")=name("last")
+ if $data(name("first")) set ^zmcmData("children",nextChild,"name","first")=name("first")
+ if $data(name("middle")) set ^zmcmData("children",nextChild,"name","middle")=name("middle")
+ if $data(name("last")) set ^zmcmData("children",nextChild,"name","last")=name("last")
  set ^zmcmData("children",nextChild,"dateOfBirth")=dateOfBirth
  set ^zmcmData("children",nextChild,"measurements")=""
  set ^zmcmData("children",nextChild,"measurements","weigth")=""
  set ^zmcmData("children",nextChild,"measurements","height")=""
  set ^zmcmData("children",nextChild,"measurements","headCircumference")=""
  set ^zmcmData("nextChild")=nextChild+1
- quit ""
+ quit
  ;
 addWeightMeasurement(childId,date,weight) ;
- set ^zmcmData("children",nextChild,"measurements","weigth",date)=weight
- quit ""
+ set ^zmcmData("children",childId,"measurements","weigth",date)=weight
+ quit
  ;
 addHeightMeasurement(childId,date,height) ;
- set ^zmcmData("children",nextChild,"measurements","heigth",date)=height
- quit ""
+ set ^zmcmData("children",childId,"measurements","heigth",date)=height
+ quit
  ;
 addHeadCircMeasurement(childId,date,headCircumference) ;
- set ^zmcmData("children",nextChild,"measurements","headCircumference",date)=headCircumference
- quit ""
+ set ^zmcmData("children",childId,"measurements","headCircumference",date)=headCircumference
+ quit
  ;
