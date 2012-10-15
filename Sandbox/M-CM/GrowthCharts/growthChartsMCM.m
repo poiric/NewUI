e6aa9bd924bc70ad631708148e054010a37d7631
Luis Ibanez <luis.ibanez@kitware.com>
2012-10-14 18:28:58 -0600

    Focus on weight and time.

--- Sandbox/M-CM/GrowthCharts/growthChartsMCM.m
+++ Sandbox/M-CM/GrowthCharts/growthChartsMCM.m
@@ -1,4 +1,4 @@
-getChartData(sessid) ; 10/14/12 5:57pm
+getChartData(sessid) ; 10/14/12 6:27pm
  new chart
  ;
  set chart("Weight",1)=10
@@ -6,20 +6,11 @@ getChartData(sessid) ; 10/14/12 5:57pm
  set chart("Weight",3)=5
  set chart("Weight",4)=2
  set chart("Weight",5)=4.7
- set chart("Height",1)=4
- set chart("Height",2)=3
- set chart("Height",3)=7
- set chart("Height",4)=5
- set chart("Height",5)=7
- set chart("month",1)="Jan"
- set chart("month",2)="Feb"
- set chart("month",3)="Mar"
- set chart("month",4)="Apr"
- set chart("month",5)="May"
+ set chart("Date",1)="01/06/2010"
+ set chart("Date",2)="01/07/2010"
+ set chart("Date",3)="01/08/2010"
+ set chart("Date",4)="01/10/2010"
+ set chart("Date",5)="01/01/2011"
  do mergeArrayToSession^%zewdAPI(.chart,"myChart",sessid)
  quit ""
- ;
-getChartPoint(sessid) ;
- do copyRequestValueToSession^%zewdAPI("month",sessid)
- do copyRequestValueToSession^%zewdAPI("score",sessid)
- quit ""
+
