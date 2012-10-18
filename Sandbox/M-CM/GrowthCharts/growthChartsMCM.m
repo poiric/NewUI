7b0fb53d942bc9933f18c30056dd3bebfcb9f2fa
Luis Ibanez <luis.ibanez@kitware.com>
2012-10-18 05:14:30 -0600

    Fixing childId

--- Sandbox/M-CM/GrowthCharts/growthChartsMCM.m
+++ Sandbox/M-CM/GrowthCharts/growthChartsMCM.m
@@ -1,16 +1,18 @@
-getChieldHeader(sessid) ; 10/18/12 4:13am
+getChildHeader(sessid) ; 10/18/12 4:51am
  new dateOfBirth
  new firstName
  new lastName
  new childName
  new childData
+ new childId
  new dateOfBirthNumeric
  new dateOfBirthText
- set dateOfBirthNumeric=^zmcmData("children",0,"dateOfBirth")
+ set childId=1
+ set dateOfBirthNumeric=^zmcmData("children",childId,"dateOfBirth")
  set dateOfBirthText=$ZDATE(dateOfBirthNumeric)
  ;
- set firstName=^zmcmData("children",0,"name","first")
- set lastName=^zmcmData("children",0,"name","last")
+ set firstName=^zmcmData("children",childId,"name","first")
+ set lastName=^zmcmData("children",childId,"name","last")
  set childName=firstName_" "_lastName
  set childData="Name: "_childName_"       Date of Birth: "_dateOfBirthText
  do setSessionValue^%zewdAPI("childData",childData,sessid)
@@ -25,13 +27,13 @@ getWeightData(sessid) ; 10/18/12 3:44am
  new chart
  new childId
  new measureDate
- set childId=0
+ set childId=1
  new dateOfBirth
  new daysOfLife
- set dateOfBirth=^zmcmData("children",0,"dateOfBirth")
+ set dateOfBirth=^zmcmData("children",childId,"dateOfBirth")
  set measureDate=""
  for  set measureDate=$ORDER(^zmcmData("children",childId,"measurements","weight",measureDate))  quit:measureDate=""  do
- . set measurement=^zmcmData("children",0,"measurements","weight",measureDate)
+ . set measurement=^zmcmData("children",childId,"measurements","weight",measureDate)
  . set daysOfLife=measureDate-dateOfBirth
  . set chart("Weight",daysOfLife)=measurement
  . set chart("Date",daysOfLife)=daysOfLife
@@ -46,13 +48,13 @@ getHeightData(sessid) ; 10/18/12 3:22am
  new chart
  new childId
  new measureDate
- set childId=0
+ set childId=1
  new dateOfBirth
  new daysOfLife
- set dateOfBirth=^zmcmData("children",0,"dateOfBirth")
+ set dateOfBirth=^zmcmData("children",childId,"dateOfBirth")
  set measureDate=""
  for  set measureDate=$ORDER(^zmcmData("children",childId,"measurements","height",measureDate))  quit:measureDate=""  do
- . set measurement=^zmcmData("children",0,"measurements","height",measureDate)
+ . set measurement=^zmcmData("children",childId,"measurements","height",measureDate)
  . set daysOfLife=measureDate-dateOfBirth
  . set chart("Height",daysOfLife)=measurement
  . set chart("Date",daysOfLife)=daysOfLife
@@ -67,13 +69,13 @@ getHeadCircData(sessid) ; 10/14/12 6:36pm
  new chart
  new childId
  new measureDate
- set childId=0
+ set childId=1
  new dateOfBirth
  new daysOfLife
- set dateOfBirth=^zmcmData("children",0,"dateOfBirth")
+ set dateOfBirth=^zmcmData("children",childId,"dateOfBirth")
  set measureDate=""
  for  set measureDate=$ORDER(^zmcmData("children",childId,"measurements","headCircumference",measureDate))  quit:measureDate=""  do
- . set measurement=^zmcmData("children",0,"measurements","headCircumference",measureDate)
+ . set measurement=^zmcmData("children",childId,"measurements","headCircumference",measureDate)
  . set daysOfLife=measureDate-dateOfBirth
  . set chart("HeadCirc",daysOfLife)=measurement
  . set chart("Date",daysOfLife)=daysOfLife
