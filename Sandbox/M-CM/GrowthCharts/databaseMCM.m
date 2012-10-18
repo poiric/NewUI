9e7597dbdbf1f573ba2ae9db1c5e3119ce27c8c6
Luis Ibanez <luis.ibanez@kitware.com>
2012-10-17 23:22:18 -0600

    Added gender and fixed date format

--- Sandbox/M-CM/GrowthCharts/databaseMCM.m
+++ Sandbox/M-CM/GrowthCharts/databaseMCM.m
@@ -1,34 +1,44 @@
-createDB() ; 10/14/12 8:00pm
+createDB() ; 10/17/12 11:14pm
  kill ^zmcmData
  set ^zmcmData=""
  set ^zmcmData("nextChild")=0
  set ^zmcmData("children")=""
  quit
  ;
-addChild(name,dateOfBirth) ;
- new nextChild
- set nextChild=^zmcmData("nextChild")
- set ^zmcmData("children",nextChild,"name")=""
- if $data(name("first")) set ^zmcmData("children",nextChild,"name","first")=name("first")
- if $data(name("middle")) set ^zmcmData("children",nextChild,"name","middle")=name("middle")
- if $data(name("last")) set ^zmcmData("children",nextChild,"name","last")=name("last")
- set ^zmcmData("children",nextChild,"dateOfBirth")=dateOfBirth
- set ^zmcmData("children",nextChild,"measurements")=""
- set ^zmcmData("children",nextChild,"measurements","weigth")=""
- set ^zmcmData("children",nextChild,"measurements","height")=""
- set ^zmcmData("children",nextChild,"measurements","headCircumference")=""
- set ^zmcmData("nextChild")=nextChild+1
- quit
+addChild(name,dateOfBirth,gender) ;
+ new thisChild
+ set thisChild=^zmcmData("nextChild")
+ set ^zmcmData("children",thisChild,"name")=""
+ if $data(name("first")) set ^zmcmData("children",thisChild,"name","first")=name("first")
+ if $data(name("middle")) set ^zmcmData("children",thisChild,"name","middle")=name("middle")
+ if $data(name("last")) set ^zmcmData("children",thisChild,"name","last")=name("last")
+ set ^zmcmData("children",thisChild,"dateOfBirth")=$$FUNC^%DATE(dateOfBirth)
+ set ^zmcmData("children",thisChild,"gender")=gender
+ set ^zmcmData("children",thisChild,"measurements")=""
+ set ^zmcmData("children",thisChild,"measurements","weigth")=""
+ set ^zmcmData("children",thisChild,"measurements","height")=""
+ set ^zmcmData("children",thisChild,"measurements","headCircumference")=""
+ set ^zmcmData("nextChild")=thisChild+1
+ quit thisChild
  ;
 addWeightMeasurement(childId,date,weight) ;
- set ^zmcmData("children",childId,"measurements","weigth",date)=weight
+ new horologDate
+ set horologDate=$$FUNC^%DATE(date)
+ set ^zmcmData("children",childId,"measurements","weigth",horologDate)=weight
+ kill horologDate
  quit
  ;
 addHeightMeasurement(childId,date,height) ;
- set ^zmcmData("children",childId,"measurements","heigth",date)=height
+ new horologDate
+ set horologDate=$$FUNC^%DATE(date)
+ set ^zmcmData("children",childId,"measurements","heigth",horologDate)=height
+ kill horologDate
  quit
  ;
 addHeadCircMeasurement(childId,date,headCircumference) ;
- set ^zmcmData("children",childId,"measurements","headCircumference",date)=headCircumference
+ new horologDate
+ set horologDate=$$FUNC^%DATE(date)
+ set ^zmcmData("children",childId,"measurements","headCircumference",horologDate)=headCircumference
+ kill horologDate
  quit
  ;
