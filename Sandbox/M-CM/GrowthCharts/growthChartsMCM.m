42fc5cb2341941ab627ccb9cb835d14714b77e51
Luis Ibanez <luis.ibanez@kitware.com>
2012-10-18 01:00:58 -0600

    Now getting weight data from database

--- Sandbox/M-CM/GrowthCharts/growthChartsMCM.m
+++ Sandbox/M-CM/GrowthCharts/growthChartsMCM.m
@@ -1,16 +1,30 @@
-getWeightData(sessid) ; 10/14/12 6:42pm
+writeWeightData() ; 10/18/12 1:00am
  new chart
+ new childId
+ new weightDate
+ set childId=0
+ set weightDate="" for  set weightDate=$ORDER(^zmcmData("children",0,"measurements","weight",weightDate))  quit:weightDate=""  do
+ . set weightEntry=^zmcmData("children",0,"measurements","weight",weightDate)
+ . write weightDate," = ",weightEntry,!
+ . set chart("Weight",weightDate)=weightEntry
+ . set chart("Date",weightDate)=$ZDATE(weightDate)
+ zwrite chart
+ kill chart
+ kill childId
+ kill weightDate
+ quit
  ;
- set chart("Weight",1)=10
- set chart("Weight",2)=9
- set chart("Weight",3)=5
- set chart("Weight",4)=2
- set chart("Weight",5)=4.7
- set chart("Date",1)="01/06/2010"
- set chart("Date",2)="01/07/2010"
- set chart("Date",3)="01/08/2010"
- set chart("Date",4)="01/10/2010"
- set chart("Date",5)="01/01/2011"
+getWeightData(sessid) ; 10/18/12 12:46am
+ new chart
+ new childId
+ new weightDate
+ set childId=0
+ set weightDate="" for  set weightDate=$ORDER(^zmcmData("children",0,"measurements","weight",weightDate))  quit:weightDate=""  do
+ . set weightEntry=^zmcmData("children",0,"measurements","weight",weightDate)
+ . write weightDate," = ",weightEntry,!
+ . set chart("Weight",weightDate)=weightEntry
+ . set chart("Date",weightDate)=$ZDATE(weightDate)
+ ;
  do mergeArrayToSession^%zewdAPI(.chart,"weightChart",sessid)
  quit ""
  ;
