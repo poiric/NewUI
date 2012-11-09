e83d1a6c1cbde4e998169e4fb8e7a5f03a31837a
pianodistance <piano.distance@gmail.com>
2012-11-09 11:35:03 -0500

    Update Sandbox/M-CM/GrowthCharts/pTable.m
    I just put a pilot database so that I could know how it works. Fortunately, I could contribute to my part.

--- Sandbox/M-CM/GrowthCharts/pTable.m
+++ Sandbox/M-CM/GrowthCharts/pTable.m
@@ -1,2 +1,11 @@
-; Start here
-
+getGridData(sessid)
+;
+n data
+;
+s data(1,"participant")="Participant 1"
+s data(1,"sex")="Male"
+s data(1,"age beyond 3")="Yes"
+d deleteFromSession^%zewdAPI("Table",sessid)
+d mergeArrayToSession^%zewdAPI(.data,"Table",sessid)
+;
+QUIT ""
