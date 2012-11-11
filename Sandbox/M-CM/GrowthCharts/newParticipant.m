2c4749d93e9766a8be06d6ba4210c69fd29eead3
Luis Ibanez <luis.ibanez@kitware.com>
2012-11-10 18:09:20 -0700

    Adding logic to validate new participant.
    Still needs more work.

--- Sandbox/M-CM/GrowthCharts/newParticipant.m
+++ Sandbox/M-CM/GrowthCharts/newParticipant.m
@@ -1,8 +1,14 @@
-getSexList(sessid) ; 11/10/12 5:17pm
-;
+getSexList(sessid) ; 11/10/12 5:27pm
+ ;
  do clearList^%zewdAPI("sex",sessid)
  do appendToList^%zewdAPI("sex","Male","male",sessid)
  do appendToList^%zewdAPI("sex","Female","female",sessid)
  do setSessionValue^%zewdAPI("sex","male",sessid)
+ ;
+ QUIT ""
+ ;
+validateNewParticipant(sessid) ;
+ ; 
+ s value=$$getSessionValue^%zewdAPI("rank",sessid)
  ;
  QUIT ""
