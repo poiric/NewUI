b3c7c14206e6b7b2b7e0332b0a25b1801dd800f8
Luis Ibanez <luis.ibanez@kitware.com>
2012-11-18 16:12:11 -0500

    Scripts for setting directories and git repos.
    Create directories and git repositories needed for EWD configuration.

---
+++ Sandbox/M-CM/GrowthCharts/Configuration/cloneRepositories.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+#
+# To be called after createDirectories.sh
+#
+cd ~/src
+git clone git://github.com/robtweed/EWD.git
+git clone git://github.com/OSEHR/NewUI.git
+
---
+++ Sandbox/M-CM/GrowthCharts/Configuration/createDirectories.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+#
+# Create directories
+#
+mkdir -p ~/src
+mkdir -p ~/ewd-instance
+mkdir -p ~/ewd-instance/r
+mkdir -p ~/ewd-instance/g
