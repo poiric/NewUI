943384ffdff4cc4b760be6faa0f7a16084534ec3
Luis Ibanez <luis.ibanez@kitware.com>
2012-11-18 19:43:25 -0500

    Adjust to install GT.M 5.5-000

--- Sandbox/M-CM/GrowthCharts/Configuration/installGTM.sh
+++ Sandbox/M-CM/GrowthCharts/Configuration/installGTM.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
-wget http://sourceforge.net/projects/fis-gtm/files/GT.M%20Installer/v0.12/gtminstall
-/bin/sh ./gtminstall
+
+# 
+# wget http://sourceforge.net/projects/fis-gtm/files/GT.M%20Installer/v0.12/gtminstall
+# /bin/sh ./gtminstall
+#
+
+wget -q -N http://sourceforge.net/projects/fis-gtm/files/GT.M-amd64-Linux/V5.5-000/gtm_V55000_linux_x8664_pro.tar.gz
+mkdir fisgtm-v55000
+cd fisgtm-v55000
+tar -xzf ../gtm_V55000_linux_x8664_pro.tar.gz
+
